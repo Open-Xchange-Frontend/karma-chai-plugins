9278af2e330dcd8f6d46c42c4ac905917c5b6a8d
Julian Bäume <julian@svg4all.de>
2013-09-20 15:44:51 +0200

    remove ability to use karma-requirejs
    There are quite some problems using this plugin with karma-requirejs.
    Using this without require works much more reliable.
    
    However, you have to be careful about the order of the frameworks you
    register with karma. Make sure to register 'chai' after
    'chai-as-promised', because chai-as-promised depends on chai to be
    loaded. The next step will be to handle the load order in a more
    intelligent way.

--- index.js
+++ index.js
@@ -1,92 +1,33 @@
 var path = require('path');
 var fs = require('fs');
 
-// TODO Do not expect require to be included before plugins
-var requireUsed = function(files) {
-    if (files) {
-        for (var i = files.length, pattern; i > 0; i--) {
-            pattern = files[i] && files[i].pattern;
-
-            if (typeof pattern === 'string' && pattern.indexOf('/karma-requirejs/') !== -1) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-};
-
-var pattern = function(file, required) {
-    return {pattern: file, included: !required, served: true, watched: false};
-};
-
-var requireAdapter = function(plugin, filePath, files, use) {
-    var name = path.join(__dirname, '_' + plugin + '-require-adapter.js');
-
-    var content  = 'requirejs.config({\n' +
-                   '  "paths": {\n' +
-                   '    "' + plugin + '": "/absolute' + path.resolve(filePath).replace(/\.js$/,'') + '"\n' +
-                   '  }\n' +
-                   '});\n\n';
-
-    if (use) {
-        content += 'require(["chai", "' + plugin + '"], function(chai, plugin){\n' +
-                   '    chai.use(plugin);\n' +
-                   '});';
-    } else {
-        content += 'require(["chai"], function(chai){\n' +
-                   '    chai.should();\n' +
-                   '});';
-    }
-
-    fs.writeFileSync(name, content);
-
-    files.push(pattern(name));
+var pattern = function(file) {
+    return {pattern: file, included: true, served: true, watched: false};
 };
 
 var plugins = {
     'chai': function(name, files) {
         var filePath = path.resolve(require.resolve('chai'), '../chai.js');
-        var required = requireUsed(files);
 
-        if(!required) {
-            files.unshift(pattern(path.join(__dirname, 'chai-adapter.js')));
-        } else {
-            requireAdapter(name, filePath, files);
-        }
-
-        files.unshift(pattern(filePath, required));
+        files.unshift(pattern(path.join(__dirname, 'chai-adapter.js')));
+        //TODO: may be, make sure, this is added before *all* other chai plugins
+        files.unshift(pattern(filePath));
     },
     'chai-as-promised': function(name, files) {
         var filePath = require.resolve(name);
-        var required = requireUsed(files);
 
-        if (required) {
-            requireAdapter(name, filePath, files, true);
-        }
-
-        files.push(pattern(filePath, required));
+        files.unshift(pattern(filePath));
     },
     'sinon-chai': function(name, files) {
         var filePath = require.resolve(name);
-        var required = requireUsed(files);
 
-        if (required) {
-            requireAdapter(name, filePath, files, true);
-        }
-
-        files.push(pattern(filePath, required));
+        files.unshift(pattern(filePath));
         files.unshift(pattern(path.resolve(require.resolve('sinon'), '../../pkg/sinon.js')));
     },
     'chai-jquery': function(name, files) {
         var filePath = require.resolve(name);
-        var required = requireUsed(files);
 
-        if (required) {
-            requireAdapter(name, filePath, files, true);
-        }
-
-        files.push(pattern(filePath, required));
+        files.unshift(pattern(filePath));
     }
 };
 
